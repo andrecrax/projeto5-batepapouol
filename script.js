9f86db37888a1a02a66fe2c8c7b76345f2c5e1f5
André Craco <49844995+andrecrax@users.noreply.github.com>
2023-01-22 19:53:49 -0300

    busca de mensagens

---
+++ script.js
@@ -0,0 +1,60 @@
+let userName;
+let stat;
+const divMsg = document.getElementById("mensagens");
+
+
+function loginError(){
+    stat = 400;
+    window.location.reload();
+}
+function loginSucess(){
+    stat = 200;
+    setInterval(manterConexao, 5000);
+    setInterval(buscarMensagens, 3000);
+}
+function manterConexao() {
+    axios.post(`https://mock-api.driven.com.br/api/v6/uol/status`, userName);
+}
+function tipoMsg(mensagem) {
+    if (mensagem.type === `message`) {
+        divMsg.innerHTML += `<div data-test="message">
+        <b>(${mensagem.time})&nbsp;</b> <span>${mensagem.from}&nbsp;</span> para&nbsp; <span>
+        ${mensagem.to}</span>: ${mensagem.text}
+        </div>`;
+    } else if (mensagem.type === `status`) {
+        divMsg.innerHTML += `<div class="statusMsg" data-test="message">
+        <b>(${mensagem.time})&nbsp;</b><span>${mensagem.from}&nbsp;</span>${mensagem.text}
+        </div>`;
+    } else {
+        divMsg.innerHTML += `<div class="privateMsg" data-test="message">
+        <b>(${mensagem.time})&nbsp;</b> <span>${mensagem.from}&nbsp;</span> reservadamente para&nbsp; <span>
+        ${mensagem.to}</span>: ${mensagem.text}
+        </div>`;
+    }
+}
+function exibirMsg(msg){
+    divMsg.innerHTML = "";
+    msg.forEach(tipoMsg);
+    divMsg.lastChild.scrollIntoView(true);
+}
+
+function buscarMensagens() {
+    const promisse = axios.get(`https://mock-api.driven.com.br/api/v6/uol/messages`);
+    promisse.then((msg) => {
+        const message = msg.data.filter(Msg => Msg.type === `message` || Msg.type === `status`
+        || (Msg.type === `private_message` & (Msg.from === userName || Msg.to === userName)));
+    exibirMsg(message);
+    });
+}
+
+function login() {
+    let string = prompt("Digite seu nome");
+    userName = { name: `${string}` };
+
+    const promisse = axios.post(`https://mock-api.driven.com.br/api/v6/uol/participants`, userName);
+
+    promisse.then(loginSucess);
+    promisse.catch(loginError);
+}
+
+login();
